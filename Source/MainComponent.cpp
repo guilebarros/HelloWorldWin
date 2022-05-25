ff2ac650934dd2aff02d4517554d3ffad8a4abb8
guilebarros <barros.guilherme@gmail.com>
2022-05-25 11:16:33 -0300

    Hello Original

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,9 +16,9 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (48.0f));
+    g.setFont (juce::Font (38.0f));
     g.setColour (juce::Colours::powderblue);
-    g.drawText ("New Branch Updates", getLocalBounds(), juce::Justification::centred, true);
+    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
